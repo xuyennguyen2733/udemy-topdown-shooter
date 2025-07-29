2acfc27808ce85866aeb5b89adc5231ff0c3e978
THI TRIEU XUYEN NGUYEN <u1252970@umail.utah.edu>
2025-07-29 16:03:32 -0600

    Full tank and tower movement implemented

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,24 +4,83 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float moveSpeed;
-    public Rigidbody rb;
-    public float verticalInput;
+    private Rigidbody rb;
 
-    // Start is called before the first frame update
-    void Start()
+    [Header("Movement data")]
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float rotationSpeed;
+
+    private float verticalInput;
+    private float horizontalInput;
+
+    [Header("Tower data")]
+    [SerializeField] private Transform towerTransform;
+    [SerializeField] private float towerRotationSpeed;
+
+    [Header("Aim data")]
+    [SerializeField] private LayerMask whatIsAimMask;
+    [SerializeField] private Transform aimTransform;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        UpdateAim();
+        CheckInput();
+    }
+
+    private void CheckInput()
+    {
         verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if (verticalInput < 0)
+        {
+            horizontalInput = -Input.GetAxis("Horizontal");
+        }
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector3(0, 0, moveSpeed * verticalInput);
+        ApplyMovement();
+        ApplyBodyRotation();
+        ApplyTowerRotation();
+    }
+
+    private void ApplyTowerRotation()
+    {
+        Vector3 direction = aimTransform.position - towerTransform.position;
+        direction.y = 0;
+
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+
+        towerTransform.rotation = Quaternion.RotateTowards(towerTransform.rotation, targetRotation, towerRotationSpeed);
+    }
+
+    private void ApplyBodyRotation()
+    {
+        transform.Rotate(0, horizontalInput * rotationSpeed, 0);
+    }
+
+    private void ApplyMovement()
+    {
+        Vector3 movement = transform.forward * moveSpeed * verticalInput;
+        rb.velocity = movement;
+    }
+
+    private void UpdateAim()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, whatIsAimMask))
+        {
+            float fixedY = aimTransform.position.y;
+            aimTransform.position = new Vector3(hit.point.x, fixedY, hit.point.z);
+        }
     }
 }
